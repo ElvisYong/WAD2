7707f8df775070afe1a32f1f7253d1cdd4be92c3
elvisyong <elvisyong@gmail.com>
2022-09-30 13:10:37 +0800

    feat: cleanup oauth

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { createHead } from '@vueuse/head'
 import { createAuth0 } from '@auth0/auth0-vue'
+import auth from './auth'
 
 const app = createApp(App)
 const head = createHead()
@@ -12,14 +13,7 @@ const router = createRouter({
   history: createWebHistory(),
 })
 
-app.use(
-  createAuth0({
-    domain: import.meta.env.AUTH_DOMAIN,
-    client_id: import.meta.env.AUTH_CLIENT_ID,
-    redirect_uri: window.location.origin,
-  })
-)
-
+app.use(auth)
 app.use(router)
 app.use(head)
 app.mount(document.body)
