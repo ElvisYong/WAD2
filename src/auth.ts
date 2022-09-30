41399e46dcc60932dbc6e6c28cd8e2c7be77d2a9
elvisyong <elvisyong@gmail.com>
2022-09-30 13:28:21 +0800

    feat: add auth guard when loggin in

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,8 @@
 import { createAuth0 } from "@auth0/auth0-vue";
 
 const auth = createAuth0({
-  domain: import.meta.env.AUTH_DOMAIN,
-  client_id: import.meta.env.AUTH_CLIENT_ID,
+  domain: import.meta.env.VITE_AUTH_DOMAIN,
+  client_id: import.meta.env.VITE_AUTH_CLIENT_ID,
   redirect_uri: window.location.origin,
 });
 
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,12 @@
 import { createApp } from 'vue'
 import './tailwind.css'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router/auto'
 import { createHead } from '@vueuse/head'
-import { createAuth0 } from '@auth0/auth0-vue'
 import auth from './auth'
+import router from './router'
 
 const app = createApp(App)
 const head = createHead()
-
-const router = createRouter({
-  history: createWebHistory(),
-})
 
 app.use(auth)
 app.use(router)
