86722a53fcd3ccb4ed02f3a858db2043f46160c3
elvisyong <elvisyong@gmail.com>
2022-10-16 14:13:44 +0800

    feat: Add a basic carousel slider

---
+++ src/components/home/PreferenceCarousel.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide, useSwiper, } from "swiper/vue";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+const modules = [Navigation, Pagination];
+
+</script>
+<!-- TODO: Finish up carousel component with actual content -->
+<template>
+  <Swiper :pagination="{
+    type: 'progressbar',
+  }" :navigation="true" :modules="modules" class="text-center w-full h-full">
+    <SwiperSlide>
+      <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+    </SwiperSlide>
+    <SwiperSlide>
+      <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+    </SwiperSlide>
+    <SwiperSlide>
+      <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+    </SwiperSlide>
+    <SwiperSlide>
+      <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+    </SwiperSlide>
+  </swiper>
+</template>
