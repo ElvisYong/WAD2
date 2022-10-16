5c550752a28bd6821cd5663ef1c4dce324f39389
elvisyong <elvisyong@gmail.com>
2022-10-16 15:42:17 +0800

    feat: Add watcher for navigation change

--- src/components/nav/NavSideBar.vue
+++ src/components/nav/NavSideBar.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
 import { useAuth0 } from "@auth0/auth0-vue";
+import { onMounted, watch } from "vue";
+import { useRoute } from 'vue-router';
 
 const {
   loginWithPopup, logout, user, isAuthenticated,
 } = useAuth0();
 
 const getProfileImage = () => (user.value.picture ? user.value.picture : "");
-
 const logoutOnClick = () => logout({ returnTo: window.location.origin });
+const route = useRoute();
+const routeName = ref()
+
+watch(
+  () => route.name,
+  () => {
+    routeName.value = route.name;
+  }
+);
+
+
 </script>
 
 <template>
@@ -24,7 +36,7 @@ const logoutOnClick = () => logout({ returnTo: window.location.origin });
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
             </svg>
           </label>
-          <a class="btn btn-ghost normal-case text-xl">CookBookie</a>
+          <a class="normal-case text-xl">CookBookie</a>
         </div>
       </div>
       <slot></slot>
@@ -35,24 +47,41 @@ const logoutOnClick = () => logout({ returnTo: window.location.origin });
       <ul class="menu p-4 overflow-y-auto w-50 bg-base-200 text-base-content">
         <!-- Sidebar content here -->
         <div class="mt-3">
-
-          <a class="btn btn-ghost normal-case text-xl ">CookBookie</a>
-          <div v-if="isAuthenticated">
-            <li>
-              <div class="avatar center">
-                <div class="w-16 rounded">
-                  <img :src="getProfileImage()" alt="" />
+          <!-- Logo and login -->
+          <div>
+            <!-- TODO: Add icon -->
+            <a class="normal-case text-xl ">CookBookie</a>
+            <div v-if="isAuthenticated">
+              <li>
+                <div class="avatar center">
+                  <div class="w-16 rounded">
+                    <img :src="getProfileImage()" alt="" />
+                  </div>
                 </div>
-              </div>
-              <a>{{user.name}}</a>
-            </li>
-            <li @click="logoutOnClick"><a>Log out</a></li>
+                <a>{{user.name}}</a>
+              </li>
+              <li @click="logoutOnClick"><a>Log out</a></li>
+            </div>
+
+            <div v-else>
+              <li class="btn btn-sm btn-primary text-white" @click="loginWithPopup">
+                Sign up / Log In
+              </li>
+            </div>
           </div>
-          <div v-else>
-            <li class="btn btn-sm btn-primary text-white" @click="loginWithPopup">
-              Sign up / Log In
+
+          <!-- TODO: Update and add all the required pages -->
+          <!-- List of pages -->
+          <div class="space-y-1">
+            <li class="mt-3">
+              <a :class="routeName === 'Home' && 'active'">Home</a>
+            </li>
+            <li>
+              <a :class="routeName === 'About' && 'active'">About</a>
             </li>
           </div>
+
+          <!-- TODO: Add user specific actions here -->
         </div>
       </ul>
 
