8b0aa2da9205fd630c0819ba7127bc2e79bdfcc9
elvisyong <elvisyong@gmail.com>
2022-10-24 21:43:15 +0800

    style: add text hover colors

--- src/components/home/CardItem.vue
+++ src/components/home/CardItem.vue
@@ -3,24 +3,31 @@ import { useRouter, useRoute } from 'vue-router'
 import { defineProps, ref } from 'vue';
 
 const router = useRouter()
-const route = useRoute()
-
 const props = defineProps(['recipe'])
 const recipe = ref(props.recipe)
 
+const gotoRecipeInfo = (recipe) => {
+  router.push(
+    {
+      path: '/recipe-info/:id', name: 'RecipeInfo',
+      params: { id: recipe.id },
+      props: { recipe: recipe }
+    })
+}
 
 </script>
 
 <!-- TODO: Style cards -->
 <template>
-  <div>
-    <div class="avatar">
+  <div @click="gotoRecipeInfo(recipe)">
+    <div class="avatar cursor-pointer">
       <div class="rounded">
         <img :src="recipe.image" alt="Shoes" />
       </div>
     </div>
+
     <div class="flex">
-      <p class="text-sm">{{ recipe.title }}</p>
+      <p class="cursor-pointer text-gray-600 hover:text-yellow-500">{{ recipe.title }}</p>
     </div>
   </div>
 </template>
