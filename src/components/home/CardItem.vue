02138fb82dbdc17c1de688585779cdbbded9d73e
junyiiii <junyi.low.2021@scis.smu.edu.sg>
2022-10-26 00:57:23 +0800

    hover
    ps this is the one jn idk if i managed to commit anything oops

--- src/components/home/CardItem.vue
+++ src/components/home/CardItem.vue
@@ -21,8 +21,13 @@ const gotoRecipeInfo = (recipe) => {
 <template>
   <div @click="gotoRecipeInfo(recipe)">
     <div class="avatar cursor-pointer">
-      <div class="rounded">
-        <img :src="recipe.image" alt="Shoes" />
+      <div class="w-40 h-40 relative"> <!--unfortunately if i dont define a height and width the images doesnt work:(-->
+        <div class="absolute inset-0 bg-cover bg-center z-0">
+          <img class="rounded" :src="recipe.image" alt="recipeimage" />
+        </div>
+        <div class="opacity-0 hover:opacity-60 absolute inset-0 flex justify-center text-sm text-white py-2 bg-black items-center">
+          {{recipe.extendedIngredients[0].name}}
+        </div>
       </div>
     </div>
 
