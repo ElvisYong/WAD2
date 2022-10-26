8f4187773ac2dfa408e61f5afa4d85d8a51ddb5e
elvisyong <elvisyong@gmail.com>
2022-10-26 16:13:59 +0800

    feat: scaffold basic summary view

---
+++ src/components/recipe-info/Summary.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps(['recipe'])
+console.log(props.recipe)
+const recipe = ref(props.recipe)
+
+</script>
+
+<template>
+  <div class="flex justify-between">
+    <div>
+      <h1 class="font-bold text-2xl">{{ recipe.title }}</h1>
+      <TextLink>{{recipe.sourceName}}</TextLink>
+    </div>
+
+    <div>
+      <div class="avatar">
+        <div class="w-96 rounded">
+          <img :src="recipe.image" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
