3a3032a7f41cbffa07582140542d716674a5fd56
elvisyong <elvisyong@gmail.com>
2022-10-26 15:27:26 +0800

    feat: use deployed api endpoint

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,1 +1,2 @@
-export const API_ENDPOINT = "http://0.0.0.0:8080";
+// This endpoint will change upon every backend redeployment as i didn't use a static endpoint
+export const API_ENDPOINT = "http://54.255.177.217:8080";
