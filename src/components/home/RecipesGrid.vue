4bca3a6cfc88e8f96e57fbcedc21ee2f29a644cd
junyiiii <junyi.low.2021@scis.smu.edu.sg>
2022-10-28 19:33:01 +0800

    changed the responsiveness
    but still ugly i think lol but does it work better now?

--- src/components/home/RecipesGrid.vue
+++ src/components/home/RecipesGrid.vue
@@ -4570,11 +4570,12 @@ onMounted(async () => {
         <div></div>
         <h1 class="ml-3 font-bold text-xl">Recipes!</h1>
       </div>
-      <div class="container grid xl:grid-cols-7 lg:grid-cols-6 md:grid-cols-5 sm:grid-cols-1">
+      <div class="container grid xl:grid-cols-11 lg:grid-cols-9 md:grid-cols-7 sm:grid-cols-1">
       <div class=""></div>
-      <div class="mt-3 p-3 grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+      <div class="mt-3 p-3 grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xl:gap-50 lg:gap-40 md:gap-40">
         <div v-for="recipe in randomRecipes">
           <CardItem :recipe="recipe" />
+          <div></div>
         </div>
       </div>
       <div class=""></div>
