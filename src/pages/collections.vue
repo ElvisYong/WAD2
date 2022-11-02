ab83a7d8b4a92525607fa5f28176e01b40cd6011
elvisyong <elvisyong@gmail.com>
2022-11-02 12:13:42 +0800

    feat: modularize recipegrid to be used in more places

---
+++ src/pages/collections.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <div class="mx-auto">
+    <div class="hidden mx-auto w-1/2 mt-6 lg:flex">
+      <SearchBar />
+    </div>
+
+    <!-- <div class="mt-10">
+      <PreferenceForm />
+    </div> -->
+
+    <div>
+      <RecipesGrid />
+    </div>
+  </div>
+</template>
