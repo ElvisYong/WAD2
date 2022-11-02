b2fdda3109ac6a95fc5b9da77f85fb7b9c586d1a
elvisyong <elvisyong@gmail.com>
2022-11-02 15:24:20 +0800

    feat: add mounted calll getUserCollectionByName

--- src/pages/collections.vue
+++ src/pages/collections.vue
@@ -1,16 +1,30 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { getUserCollectionByName } from "../apis/collections"
+
+const route = useRoute();
+const collection = ref(null)
+
+onMounted(async () => {
+  const userId = route.params.userId
+  const collectionName = route.params.name
+  try {
+    const res = await getUserCollectionByName(userId, collectionName)
+    // Time to fetch the recipes
+    if (res.data.value) {
+      collection.value = res.data.value;
+      res.data.value
+    }
+  } catch (error) {
+    console.log(error)
+  }
+});
+
 </script>
 
 <template>
   <div class="mx-auto">
-    <div class="hidden mx-auto w-1/2 mt-6 lg:flex">
-      <SearchBar />
-    </div>
-
-    <!-- <div class="mt-10">
-      <PreferenceForm />
-    </div> -->
-
     <div>
       <RecipesGrid />
     </div>
