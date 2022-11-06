ca19f804d64a875aaf30636a9a4de20b25eaf98e
elvisyong <elvisyong@gmail.com>
2022-11-06 18:14:28 +0800

    feat: scaffold some text for kitchen view

--- src/pages/my-kitchen.vue
+++ src/pages/my-kitchen.vue
@@ -6,8 +6,24 @@ useHead({
 
 <template>
     <div class="sm:mx-16 lg:mx-16 xl:mx-60">
-        <div class="hidden mx-auto w-1/2 mt-6 lg:flex">
-        <SearchBar />
+        <div class="mt-10">
+            <h1 class="text-2xl">Ready made recipes with the ingredients you have!</h1>
+            <!-- TODO: Change the text here -->
+            <p class="mt-6 text-sm">
+                It's no secret that grocery shopping can be a drag. But if you have a few ingredients on hand, we have
+                some great ways to make them into dinner.
+
+                Just enter your ingredients below and we'll show you how to prepare a meal with what you have. For
+                best results, enter 10 or more ingredients below, such as staples like salt, pepper, onion and garlic.
+            </p>
+
+            <div class="mt-6">
+                <label for="ingredients" class="block text-sm font-medium text-gray-700">Ingredients</label>
+                <div class="mt-1">
+                    <input type="text" name="ingredients" id="ingredients"
+                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+                </div>
+            </div>
         </div>
     </div>
 </template>
