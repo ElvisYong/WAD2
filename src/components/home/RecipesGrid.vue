a0f34e23f7d176b5d9ae5e4886e42497d966dbd1
elvisyong <elvisyong@gmail.com>
2022-11-11 03:54:59 +0800

    fix: RecipeGrid not being able to redirect

--- src/components/home/RecipesGrid.vue
+++ src/components/home/RecipesGrid.vue
@@ -8,32 +8,30 @@ const props = defineProps(["recipes"])
 const emits = defineEmits(["load-more"])
 const randomRecipes = ref(props.recipes)
 const el = ref(null);
-const { width, height } = useWindowSize();
+const { height } = useWindowSize();
 
-const { list, containerProps, wrapperProps } = useVirtualList(randomRecipes, {
-    itemHeight: 80,
-    overscan: 5,
+const { containerProps } = useVirtualList(randomRecipes, {
+  itemHeight: 80,
+  overscan: 5,
 });
 
 const gotoRecipeInfo = (recipe) => {
-  router.push(
-    {
-      path: '/recipe-info/:id',
-      name: '/recipe-info',
-      params: { id: recipe.id },
-    })
+  router.push({
+    path: '/recipe-info/:id',
+    name: 'RecipeInfo',
+    params: { id: recipe.id },
+  })
 }
 
 useInfiniteScroll(containerProps.ref, () => {
-   emits("load-more") 
-  }, { distance: 10 }
+  emits("load-more")
+}, { distance: 10 }
 );
 
 </script>
 
 <template>
-  <div class="mt-6 no-scrollbar" ref="el" v-bind="containerProps"
-   :style="{height: height - 200 + 'px'}">
+  <div class="mt-6 no-scrollbar" ref="el" v-bind="containerProps" :style="{ height: height - 200 + 'px' }">
     <div class="md:mx-3">
       <h1 class="mb-6 font-bold text-2xl text-center lg:text-center">Recipes</h1>
       <div class="grid md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 md:gap-10">
@@ -45,4 +43,6 @@ useInfiniteScroll(containerProps.ref, () => {
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
