0769e26a52b8c02a3e79aac03f7935966f79a9c6
junyiiii <junyi.low.2021@scis.smu.edu.sg>
2022-11-10 23:03:05 +0800

    minor edit to title:"recipe"

--- src/components/home/RecipesGrid.vue
+++ src/components/home/RecipesGrid.vue
@@ -20,7 +20,7 @@ const gotoRecipeInfo = (recipe) => {
 <template>
   <div class="mt-6">
     <div class="md:mx-3">
-      <h1 class="mb-3 font-bold text-xl text-center lg:text-left">Recipes!</h1>
+      <h1 class="mb-6 font-bold text-2xl text-center lg:text-center">Recipes</h1>
       <div class="grid md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 md:gap-10">
         <div v-for="recipe in randomRecipes" class="text-center mt-0 mb-5">
           <CardItem @click="gotoRecipeInfo(recipe)" :title="recipe.title" :image="recipe.image" />
