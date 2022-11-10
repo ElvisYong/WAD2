33e3966c70237de440cee0cfd9db7d1fa6fe5c47
junyiiii <junyi.low.2021@scis.smu.edu.sg>
2022-11-10 18:39:29 +0800

    changed the home page a lil bit with the heroes

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { getRandomRecommendations } from '../apis/recipes'
 import { useInfiniteScroll } from '@vueuse/core'
+import { useAuth0 } from "@auth0/auth0-vue";
 
 useHead({
   title: "Home",
@@ -8,6 +9,7 @@ useHead({
 
 const el = ref(null);
 const randomRecipes = ref(null);
+const { loginWithPopup, logout, user, isAuthenticated, } = useAuth0();
 
 const fetchGetRandomRecommendations = async (number) => {
   try {
@@ -44,15 +46,45 @@ useInfiniteScroll(el, async () => {
 
 <template>
   <div class="mx-auto">
-    <div class="hidden mx-auto mt-6 lg:flex">
-      <GeneralSearchBar />
+    <div class="hero min-h-screen bg-light">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <h1 v-if="!isAuthenticated" class="text-5xl font-bold">Hello!</h1>
+          <h1 v-else class="text-5xl font-bold">Welcome Back!</h1>
+          <p class="py-6">What would you like to explore today?</p>
+          <button class="btn btn-primary mr-2 lg:text-center"><a href="#recipes">Browse Recipes</a></button>
+          <button class="btn btn-primary ml-2 hidden lg:inline-block"><a href="#search">Search Recipes</a></button>
+          <!-- <button class="btn btn-primary">Get Started</button> -->
+        </div>
+      </div>
     </div>
 
-    <div v-if="randomRecipes === null" class="mt-3">
-      <Loader />
+    <div class="hero min-h-screen bg-light" id="search" style="background-image:url('src\food.jpg')">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <h1 v-if="!isAuthenticated" class="text-5xl font-bold">Hello!</h1>
+          <h1 v-else class="text-5xl font-bold">Recipe Search</h1>
+          <p class="py-6">What's cooking?</p>
+          <div class="hidden mx-auto lg:flex">
+            <GeneralSearchBar/>
+          </div>
+        </div>
+      </div>
     </div>
-    <div ref="el" v-else>
-      <RecipesGrid :recipes="randomRecipes" />
+
+    <!-- <div class="hidden mx-auto mt-6 lg:flex">
+      <GeneralSearchBar id="search"/>
+    </div> -->
+    <!-- <div v-if="!isAuthenticated">
+      <h1>Sign Up to find out more!</h1>
+    </div> -->
+    <div>
+      <div v-if="randomRecipes === null" class="mt-3">
+        <Loader />
+      </div>
+      <div ref="el" id="recipes" class="mt-3" v-else>
+        <RecipesGrid :recipes="randomRecipes" />
+      </div>
     </div>
   </div>
 </template>
