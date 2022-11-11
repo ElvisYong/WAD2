24cf1b582dea5440e5149ceaff598bf84ccabaec
elvisyong <elvisyong@gmail.com>
2022-11-11 23:32:54 +0800

    feat: add vicninty range search

--- src/pages/supermarket.vue
+++ src/pages/supermarket.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ViewColumnsIcon } from '@heroicons/vue/20/solid';
 import { onMounted } from 'vue'
 import { nearbySearch } from '../apis/gmaps'
 
@@ -7,11 +8,18 @@ const currentPos = ref()
 const latitude = ref(null)
 const longitude = ref(null)
 const markers = ref([])
+const keyword = ref("Supermarket")
+const vicinity = ref(1000)
 
 const fetchNearbySuperMarket = async () => {
   const { lat, lng } = center.value
-  const { data } = await nearbySearch("Supermarket", 1000, lat, lng, "Supermarket")
+  const { data } = await nearbySearch(keyword.value, vicinity.value, lat, lng, "Supermarket")
   const nearbySupermarkets = data.value.results
+
+  // Clear the array first
+  markers.value.splice(0, markers.value.length)
+
+
   for (const supermarket of nearbySupermarkets) {
     markers.value.push({
       position: {
@@ -43,6 +51,11 @@ watch([latitude, longitude],
   }
 )
 
+watch(vicinity, async () => {
+  markers.value = []
+  await fetchNearbySuperMarket();
+})
+
 const setPlace = (event) => {
   console.log(event)
 }
@@ -52,6 +65,24 @@ const setPlace = (event) => {
 <template>
   <div class="mt-10">
     <h1 class="text-2xl font-bold">Nearby supermarkets within your vicinity!</h1>
+
+    <div class="flex flex-col my-3">
+      <div class="flex flex-col">
+        <h2 class="font-bold">Search supermarket</h2>
+        <div>
+          <input type="text" class="input input-bordered input-primary w-80 mt-1" placeholder="Enter a keyword" />
+          <button class="btn btn-primary ml-3">GO</button>
+        </div>
+      </div>
+
+      <div class="flex gap-2 mt-3">
+        <h2>Nearby vicinity range</h2>
+        <button class="btn btn-primary btn-sm" @click="vicinity = 1000">1000m</button>
+        <button class="btn btn-primary btn-sm" @click="vicinity = 3000">3000m</button>
+        <button class="btn btn-primary btn-sm" @click="vicinity = 5000">5000m</button>
+      </div>
+    </div>
+
     <div v-if="latitude !== null && longitude !== null && markers.length > 0">
       <GMapMap :center="center" :zoom="16" map-type-id="terrain" style="width: 100vw; height: 900px">
         <GMapMarker :key="index" v-for="(m, index) in markers" :position="m.position" :clickable="true"
