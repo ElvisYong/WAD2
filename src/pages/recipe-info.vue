71780c0bbf31548d3d0fd37f2c1f1a0c8d45dc19
junyiiii <junyi.low.2021@scis.smu.edu.sg>
2022-11-13 19:19:27 +0800

    added back button on recipe page

--- src/pages/recipe-info.vue
+++ src/pages/recipe-info.vue
@@ -31,6 +31,13 @@ onMounted(async () => {
 </route>
 
 <template>
+  <a href="/">
+    <div class="ml-8 mt-8">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+      </svg>
+    </div>
+  </a>
   <div class="mx-auto max-w-4xl">
     <div v-if="recipe === null" class="mt-10">
       <Loader />
